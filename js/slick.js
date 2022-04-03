7d28063448d6f6c9e4f36210ddde26c18af57647
Demian Pashkovskyi <95377702+Demian1230@users.noreply.github.com>
2022-04-03 18:23:34 +0300

    iceCream лапки

--- js/slick.js
+++ js/slick.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.container-customer').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 1000,
+        autoplay: true,
+        autoplaySpeed: 3000,
         dots: true,
         arrows: false,
     });
