fb05892b9b3941ba088a246ba0121416cae2cf93
Demian Pashkovskyi <95377702+Demian1230@users.noreply.github.com>
2022-04-04 16:54:30 +0300

    icecream домік

--- js/slick.js
+++ js/slick.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     $(".container-customer").slick({
-        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // autoplay: true,
+        // autoplaySpeed: 3000,
         dots: true,
+        arrows: false,
 
     });
 })
