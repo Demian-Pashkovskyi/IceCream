222ae1748d5a1e3e2acf4173caef44d8b01c08c2
Demian Pashkovskyi <95377702+Demian1230@users.noreply.github.com>
2022-04-04 15:02:18 +0300

    icecream hv03

--- js/slick.js
+++ js/slick.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
-    $('.container-customer').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 3000,
-        dots: true,
+$(document).ready(function () {
+    $(".container-customer").slick({
         arrows: false,
+        dots: true,
+
     });
-});
+})
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $('.container-customer').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 3000,
+//         dots: true,
+//         arrows: false,
+//     });
+// });
 
 
 // $('.container-customer').slick({
