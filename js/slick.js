ff9c2af42c511ced41e2f09511a8a9ab89361aaa
Demian Pashkovskyi <95377702+Demian1230@users.noreply.github.com>
2022-04-07 11:02:21 +0300

    зміна точок

--- js/slick.js
+++ js/slick.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     $(".container-customer").slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 3000,
+        autoplay: true,
+        autoplaySpeed: 3000,
         dots: true,
         arrows: false,
 
@@ -15,47 +15,3 @@ $(document).ready(function () {
 
 
 
-// $(document).ready(function(){
-//     $('.container-customer').slick({
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 3000,
-//         dots: true,
-//         arrows: false,
-//     });
-// });
-
-
-// $('.container-customer').slick({
-//   dots: true,
-//   infinite: false,
-//   speed: 300,
-//   slidesToShow: 4,
-//   slidesToScroll: 4,
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         infinite: true,
-//         dots: true
-//       }
-//     },
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//       }
-//     }
-//   ]
-// });
